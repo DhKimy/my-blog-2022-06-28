bc6ac33fa20a4f89bfbed50e87ced3358416287c
dhk767 <dhk767@gmail.com>
2022-06-28 12:58:08 +0900

    version 1.2.1

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AboutPage() {
+    return <main>
+        <title>자기소개</title>
+        <h1>자기소개 페이지 임니다.</h1>
+    </main>;
+}
+
+AboutPage();
+export default AboutPage; // 펑션을 외부 파일에서 참조할 수 있게끔 함.
